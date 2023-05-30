90a3d816bd2e7245232531f2e494488714e8e989
sajjad1112ali <sajjadramzan1211@gmail.com>
2023-05-30 13:29:48 +0500

    Added validations for Items

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,10 @@
-import SignOut from "@/components/sign-out";
+import InvoiceList from "@/components/InvoiceList";
 
 export default function Home() {
   return (
       <div className="max-w-7xl w-full mx-auto">
         <h1 className="text-lime-800">Welcome Next.js</h1>
-        
+        <InvoiceList />
       </div>
   );
 }
--- components/InvoicePDF.tsx
+++ components/InvoicePDF.tsx
@@ -1,14 +1,45 @@
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
+import toast from "react-hot-toast";
 
+import { DocumentDownloadIcon } from "@heroicons/react/outline";
+import { InvoiceItem } from "@/lib/customTypes";
+
+type privateProps = {
+  items: Array<InvoiceItem>;
+}
 // Register the fonts with pdfmake
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-const InvoicePDF = ({ items }) => {
+const InvoicePDF = ({ items }: privateProps) => {
   const topMargin = 20;
-const defaultMargin = { margin: [0, 10, 0, 0] };
+  const defaultMargin = { margin: [0, 10, 0, 0] };
+
+  const isValidInput = (inputValue: string | number, name: string) => {
+    console.log(inputValue);
+    if (name === "name") return inputValue !== "";
+    return inputValue > 0;
+  };
+  const validateItems = () => {
+    return items.some((elem: InvoiceItem) => {
+      if (
+        !isValidInput(elem.name, "name") ||
+        !isValidInput(elem.qty, "count") ||
+        !isValidInput(elem.price, "count")
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  };
   const generatePDF = () => {
     // Define the document definition
+    const foundUnsatisfiedItem = validateItems();
+    if (foundUnsatisfiedItem) {
+      toast.error("Validation errors occurred for some items.");
+      return true;
+    }
     const documentDefinition = {
       content: [
         { text: "Invoice", style: "header" },
@@ -29,9 +60,9 @@ const defaultMargin = { margin: [0, 10, 0, 0] };
           columns: [
             {
               width: "*",
-              text: "Mr. Usman Sherazi..",
+              text: "Mr. Usman Sherazi",
               style: ["subheader", "fontBold"],
-              margin: [0, 30, 0, 0]
+              margin: [0, 30, 0, 0],
             },
             [
               {
@@ -54,46 +85,53 @@ const defaultMargin = { margin: [0, 10, 0, 0] };
         { text: "john@yopmail.com", style: "paragraphs" },
         { text: "Items", style: "subheader", margin: [0, 20, 0, 5] },
         generateItemsTable(items),
-        ,
-        {
-          absolutePosition: {x:456, y:20},
-          image: 'logoImage',
-        },
+        // {
+        //   absolutePosition: { x: 456, y: 20 },
+        //   image: "logoImage",
+        // },
       ],
       styles: {
-      header: { fontSize: 28, bold: true },
-      subheader: { fontSize: 14 },
-      colorOrange: { color: "#eaa352" },
-      fontBold: { bold: true },
-      paragraphs: { fontSize: 12 },
-      tableHeader: {
-        fillColor: "#eaa352",
-        color: "#FFFFFF",
-        bold: true,
-        alignment: "center",
-        fontSize: 14,
+        header: { fontSize: 28, bold: true },
+        subheader: { fontSize: 14 },
+        colorOrange: { color: "#eaa352" },
+        fontBold: { bold: true },
+        paragraphs: { fontSize: 12 },
+        tableHeader: {
+          fillColor: "#eaa352",
+          color: "#FFFFFF",
+          bold: true,
+          alignment: "center",
+          fontSize: 14,
+        },
       },
-    },
-    images: {
-        logoImage: "https://picsum.photos/100/50"
-    },
+      images: {
+        logoImage: "https://picsum.photos/100/50",
+      },
     };
 
     // Generate the PDF
     pdfMake.createPdf(documentDefinition).open();
   };
 
-  const generateItemsTable = (items) => {
+  const generateItemsTable = (items: Array<InvoiceItem>) => {
     let totalBillingAmount = 0;
     const tableBody = items.map((item) => {
       const sum = item.price * item.qty;
       totalBillingAmount += sum;
       return [
         { text: item.name, border: [false, false, false, false] },
-        { text: item.price, border: [true, false, false, false], alignment: "center", },
-        { text: item.qty, border: [false, false, false, false], alignment: "center", },
-        { text: sum, border: [true, false, false, false], alignment: "right", },
-      ]
+        {
+          text: item.price,
+          border: [true, false, false, false],
+          alignment: "center",
+        },
+        {
+          text: item.qty,
+          border: [false, false, false, false],
+          alignment: "center",
+        },
+        { text: sum, border: [true, false, false, false], alignment: "right" },
+      ];
     });
     const tableHeader = [
       {
@@ -118,20 +156,22 @@ const defaultMargin = { margin: [0, 10, 0, 0] };
       },
     ];
 
-    return [{
-      margin: [0, 10, 0, 30],
-      table: {
-        widths: [150, "*", "*", "*"],
-        headerRows: 1,
-        body: [tableHeader, ...tableBody],
+    return [
+      {
+        margin: [0, 10, 0, 30],
+        table: {
+          widths: [150, "*", "*", "*"],
+          headerRows: 1,
+          body: [tableHeader, ...tableBody],
+        },
       },
-    },
-  getTotalTable(totalBillingAmount)];
+      getTotalTable(totalBillingAmount),
+    ];
   };
 
   const getTotalTable = (billingAmount: number) => {
-    let shippingAmount = 100
-    let totalBillingAmount = billingAmount + shippingAmount
+    let shippingAmount = 100;
+    let totalBillingAmount = billingAmount + shippingAmount;
     return {
       table: {
         widths: ["*", 120], // Specify the widths of the columns
@@ -157,7 +197,7 @@ const defaultMargin = { margin: [0, 10, 0, 0] };
               alignment: "right",
             },
             {
-              text: "40",
+              text: shippingAmount,
               border: [false, false, false, false],
               alignment: "right",
             },
@@ -177,15 +217,15 @@ const defaultMargin = { margin: [0, 10, 0, 0] };
           ],
         ],
       },
-    }
-  }
+    };
+  };
 
   return (
     <button
       onClick={generatePDF}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-4 rounded"
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-3 py-2 mx-3 rounded"
     >
-      Generate PDF
+      <DocumentDownloadIcon className="h-5 w-5" />
     </button>
   );
 };
